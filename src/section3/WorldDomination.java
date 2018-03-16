464531606db2cfadf4f88938b18b1279c767d0c3
Jack <league@iMac-2.attlocal.net>
2018-03-15 19:56:26 -0700

    Dragon Fight
    magic or kick

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,18 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+String bob = JOptionPane.showInputDialog("DO U KNOW HOW TO CODE");
 		// 2. If they say "yes", tell them they will rule the world.
-
-		// 3. Otherwise, wish them good luck washing dishes.
-
+if(bob.equalsIgnoreCase("yes")) { JOptionPane.showMessageDialog(null, "U WILL RULE ZEE WORLD");
+		// 3. Otherwise, wish them good luck washing toilets.
+	
+	
+	
+	
+	
+	
+	}
+else { JOptionPane.showMessageDialog(null, "Have Fun Useing Smart Mops");
 	}
 }
-
+}
--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -5,61 +5,68 @@ import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
 public class DragonFight {
+	
+		
 	// 1. Create a main method and put all of the code below inside of it
 	public static void main(String[] args) {
 		
 		ImageIcon dragon = new ImageIcon("src/test/dragonPicture.jpg");		
 		
-		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
+		JOptionPane.showMessageDialog(null, "Defeat the Dragon King to take his treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+	int PlayerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 130;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int PlayerDamage = 10;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int dragonDamage = 10;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(PlayerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+	String ask = JOptionPane.showInputDialog("Attack: Kick or Magic")	;
 		// 9. If they typed in "yell":
-		
+		if(ask.equalsIgnoreCase("Magic")) { int x = new Random().nextInt(10)+5;
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
 			
-			//-- Subtract that number from the dragon's health variable 
-			
+			//-- Subtract that number from the dragon's health variable
+		dragonHealth = dragonHealth - x;
+		}
 		// 10. If they typed in "kick":
 		
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
+			if(ask.equalsIgnoreCase("Kick")) {int y = new Random().nextInt(10)+15;
 			//-- Subtract that number from the dragon's health variable
-			
-			
+			dragonHealth = dragonHealth - y;
+			}
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+			int p = new Random().nextInt(5)+10;
 		// 12. Subtract this number from the player's health
-		
+		PlayerHealth = PlayerHealth - p;
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if(PlayerHealth <= 0) { JOptionPane.showMessageDialog(null, "You Have Died Please Play Again");}
 			//-- Tell the user that they lost
 			
 		
 		// 14. Else if the dragon's health is less than or equal to 0
-		
+		if(dragonHealth <= 0) { JOptionPane.showMessageDialog(null, "You Have Killed The Almighty Dragon And Stole All His Treasure!");}
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
 			
 	   //  15.  Else
 			
-			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
+			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this rou
+	JOptionPane.showMessageDialog(null, "Dragon Health" + dragonHealth +"Player Health"+ PlayerHealth);
 		}
+	
+		
 	}
+	}
+
 
